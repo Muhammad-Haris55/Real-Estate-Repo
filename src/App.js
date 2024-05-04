410332e65832aa059f88f2a2f9f24aa10430140d
abdullah <abdullahmoudood35@gmail.com>
2024-05-04 14:58:46 +0500

    lazy

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
 import { Route, Routes } from "react-router-dom";
-import About from "./Component/About/About";
-import Contact from "./Component/Contact/Contact";
-import Home from "./Component/Home/Home";
 import "./index.css";
-import ClientComments from "./Component/Dashboard/ClientComments";
-import Totalprojects from "./Component/Dashboard/Totalprojects";
-import Uploading from "./Component/Dashboard/Uploading";
-import Update from "./Component/Dashboard/Update";
-import Project from "./Component/Projects/Project";
-import Innerproject from "./Component/Projects/Innerproject";
-import Userreviews from "./Component/Dashboard/Userreviews";
 import PrivateRoutes from "./Component/User/PrivaateRoutes";
 import Login1 from "./Component/loginform1/Login1";
 import Scroll from "./Component/Sidescroll/Scroll";
-
+import React, { Suspense } from "react";
+const Home1 = React.lazy(() => import("./Component/Home/Home"));
+const Project1 = React.lazy(() => import("./Component/Projects/Project"));
+const Innerproject1 = React.lazy(() =>
+  import("./Component/Projects/Innerproject")
+);
+const About1 = React.lazy(() => import("./Component/About/About"));
+const Contact1 = React.lazy(() => import("./Component/Contact/Contact"));
+const Uploading1 = React.lazy(() => import("./Component/Dashboard/Uploading"));
+const Totalprojects1 = React.lazy(() =>
+  import("./Component/Dashboard/Totalprojects")
+);
+const Update1 = React.lazy(() => import("./Component/Dashboard/Update"));
+const Userreviews1 = React.lazy(() =>
+  import("./Component/Dashboard/Userreviews")
+);
+const ClientComments1 = React.lazy(() =>
+  import("./Component/Dashboard/ClientComments")
+);
 function App() {
   return (
     <>
-    <Scroll/>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route element={<PrivateRoutes />}>
-          <Route path="/UploadProject" element={<Uploading />}></Route>
-          <Route path="/Dashboard" element={<Totalprojects />}></Route>
-          <Route path="/U" element={<Update />}></Route>
-          <Route path="/UserComments" element={<Userreviews />}></Route>
-          <Route path="/ClientReviews" element={<ClientComments />}></Route>
-        </Route>
-        <Route path="Project" element={<Project />}></Route>
-        <Route path="I" element={<Innerproject />}></Route>
-        <Route path="About" element={<About />}></Route>
-        <Route path="Contact" element={<Contact />}></Route>
-        <Route path="/Login" element={<Login1 />}></Route>  
-      </Routes>
+      <Scroll />
+      <Suspense fallback={<div> Data is Loading</div>}>
+        <Routes>
+          <Route path="/" element={<Home1 />}></Route>
+          <Route element={<PrivateRoutes />}>
+            <Route path="/UploadProject" element={<Uploading1 />}></Route>
+            <Route path="/Dashboard" element={<Totalprojects1 />}></Route>
+            <Route path="/U" element={<Update1 />}></Route>
+            <Route path="/UserComments" element={<Userreviews1 />}></Route>
+            <Route path="/ClientReviews" element={<ClientComments1 />}></Route>
+          </Route>
+          <Route path="Project" element={<Project1 />}></Route>
+          <Route path="I" element={<Innerproject1 />}></Route>
+          <Route path="About" element={<About1 />}></Route>
+          <Route path="Contact" element={<Contact1 />}></Route>
+          <Route path="/Login" element={<Login1 />}></Route>
+        </Routes>
+      </Suspense>
     </>
   );
 }
